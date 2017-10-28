84ece0d17ea2bed4d2a326edaece06b6a4e15149
Xiao Zhang <xz1661@nyu.edu>
2017-10-27 23:38:30 -0400

    init

---
+++ ArticleSpider/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'zx'
+__date__ = '10/27/17 22:00'
+
+from scrapy.cmdline import execute
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(["scrapy", "crawl", "jobbole"])
